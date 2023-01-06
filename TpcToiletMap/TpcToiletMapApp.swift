597f4928537ac9b85e907dddbb4220289890eb37
Riddle Ling <riddle.apple@gmail.com>
2023-01-06 20:38:41 +0800

    Initial Commit

---
+++ TpcToiletMap/TpcToiletMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TpcToiletMapApp.swift
+//  TpcToiletMap
+//
+//  Created by Riddle Ling on 2023/1/6.
+//
+
+import SwiftUI
+
+@main
+struct TpcToiletMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
